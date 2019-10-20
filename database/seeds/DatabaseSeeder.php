791e1953ff2ab4ac6920d3476791a22caf0cda6e
fmoraless <franticzero@gmail.com>
2019-10-20 00:28:16 -0300

    Creación de usuarios y preguntas usando Factory and seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Question;
+use App\User;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        factory(User::class, 3)->create()->each(function($u){
+            $u->questions()
+            ->saveMany(
+                factory(Question::class, rand(1, 5))->make()
+            );
+        });
+        
+
+
+
         // $this->call(UsersTableSeeder::class);
     }
 }
