f6a0da2d3e6a18cffbb9e3cf81b1cb65681cc299
fmoraless <franticzero@gmail.com>
2019-10-26 02:24:29 -0300

    Add vote controls to question and answer

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,7 @@
+import fontawesome from '@fortawesome/vue-fontawesome';
+import faCaretUp from '@fortawesome/vue-fontawesome/faCaretUp';
+import faCaretDown from '@fortawesome/vue-fontawesome/faCaretDown';
+import faStar from '@fortawesome/vue-fontawesome/faStar';
+import faCheck from '@fortawesome/vue-fontawesome/faCheck';
+
+fontawesome.library.add([faCaretUp, faCaretDown, faCheck, faStar]);
